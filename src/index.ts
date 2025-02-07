230b8bf2064a22da57ea63d2baa73f0f86d6ab54
Alfio <tomarchioalfio5@gmail.com>
2025-02-07 14:21:31 +0000

    add toString

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import * as AST from './parse/ast';
 import { parse } from './parse/parse';
 import { tokenise } from './parse/tokenise';
+import { toString } from './string/toString';
 
 export class Expression {
     ast: AST.Expression;
     constructor(source: string) {
         this.ast = parse(tokenise(source));
     };
+
+    toString(fancy = false) {
+        return toString(this.ast, fancy);
+    }
 }
---
+++ src/string/toString.ts
@@ -0,0 +1,42 @@
+import * as AST from "../parse/ast";
+
+let fancyOperator = (lexeme: string): string => {
+    switch (lexeme) {
+        case '!':
+            return '¬';
+        case '&':
+            return '∧';
+        case '|':
+            return '∨';
+        case '^':
+            return '⊕';
+        case '=>':
+            return '⇒';
+        case '<=>':
+            return '⇔';
+        default:
+            return lexeme;
+    }
+}
+
+export function toString(expression: AST.Expression, fancy = false) {
+    switch (true) {
+        case expression instanceof AST.BinaryExpression: {
+            let operator = expression.operator.lexeme;
+            if (fancy) operator = ' ' + fancyOperator(operator) + ' ';
+
+            return toString(expression.left, fancy) + operator + toString(expression.right, fancy);
+        }
+        case expression instanceof AST.UnaryExpression: {
+            let operator = expression.operator.lexeme;
+            if (fancy) operator = fancyOperator(operator);
+
+            return operator + toString(expression.right, fancy);
+        }
+        case expression instanceof AST.Grouping:
+            return '(' + toString(expression.inner, fancy) + ')';
+
+        case expression instanceof AST.Literal:
+            return expression.value.lexeme;
+    }
+}
