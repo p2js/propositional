d28e768bfd8cc183807a67dcf0b48ff1784f07bc
Alfio <tomarchioalfio5@gmail.com>
2025-02-08 17:35:38 +0000

    add generation of variable literals

--- src/syntax/generate.ts
+++ src/syntax/generate.ts
@@ -6,5 +6,6 @@ export const not = (right: AST.Expression) => new AST.UnaryExpression(new Token(
 export const and = (left: AST.Expression, right: AST.Expression) => new AST.BinaryExpression(left, new Token(TokenType.AND, '&'), right);
 export const or = (left: AST.Expression, right: AST.Expression) => new AST.BinaryExpression(left, new Token(TokenType.OR, '|'), right);
 
+export const variable = (lexeme: string) => new AST.Literal(new Token(TokenType.VARIABLE, lexeme));
 export const FALSE = new AST.Literal(new Token(TokenType.CONSTANT, "0"));
 export const TRUE = new AST.Literal(new Token(TokenType.CONSTANT, "1"));
