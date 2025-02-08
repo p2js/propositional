429f8405079c297798ca0201d8782806ca8abac2
Alfio <tomarchioalfio5@gmail.com>
2025-02-08 17:35:48 +0000

    add variable substitution and formula evaluation

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,38 @@ export class Formula {
         return new Formula(simplifyAST(this.ast));
     }
 
+    substitute(v1: string, v2: string | boolean) {
+        let l1 = variable(v1);
+        let l2 = typeof v2 == 'string' ? variable(v2) : v2 ? TRUE : FALSE;
+
+        return new Formula(substitute_variable(this.ast, l1, l2));
+    }
+
     isAtom() {
         return this.ast instanceof AST.Literal;
+    }
+
+    evaluate(values: { [key: string]: number | boolean }): boolean {
+        let ast = this.ast;
+        for (let v in values) {
+            ast = substitute_variable(ast, variable(v), !!values[v] ? TRUE : FALSE);
+        }
+        ast = simplifyAST(ast);
+        if (!(ast instanceof AST.Literal) || ast.value.type != TokenType.CONSTANT) {
+            throw new Error("Unable to evaluate, not all variables were assigned a value");
+        }
+        return ast.value.lexeme == "1";
     }
 }
 // Imports are below the class definition to resolve problems related to circular imports in the compiled output
 import * as AST from './syntax/ast';
 import { parse } from './syntax/parse';
 import { tokenise } from './syntax/tokenise';
-import { toCNF } from './cnf/cnfExpression';
+import { TRUE, FALSE, variable } from './syntax/generate';
+
 import { toString } from './transform/toString';
 import { simplifyAST } from './transform/simplify';
+import { substitute_variable } from './transform/substitute';
+
+import { toCNF } from './cnf/cnfExpression';
+import { TokenType } from './syntax/token';
---
+++ src/transform/evaluate.ts
@@ -0,0 +1,8 @@
+import * as AST from '../syntax/ast';
+import { variable } from '../syntax/generate';
+function evaluate(expression: AST.Expression, values: { [key: string]: number | boolean }): boolean {
+    for (let variable in values) {
+
+    }
+    throw "";
+}
